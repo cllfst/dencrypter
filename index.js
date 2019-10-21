23a63d374295ea950024473c9e2dfe9865289de1
Mohamed Ali ELBOURI <44344217+bouri64@users.noreply.github.com>
2019-10-21 16:34:48 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const app = express();
+app.set('view engine', 'ejs');
+
+
+app.use(bodyParser.urlencoded());
+app.get('/home',(req,res) => {
+    res.render(path.resolve("../views/index.ejs"));
+});
+const port = process.env.PORT || 8000 ;
+
+app.listen(port, () => console.log(`listening on port ${port} `));
