bcebb6903d06d7e95f4c8a11dafd9ef3e71bc458
bouri64 <bourimedali25@gmail.com>
2019-10-25 21:27:03 +0100

    added decrypt button, encrypt button creates the keys

--- src/routes/bash.sh
+++
@@ -1,6 +0,0 @@
-cd keys
-key="privateKey"
-pub="publicKey"
-`openssl genrsa -out $key 2048 1>/dev/null`
-`openssl rsa -in $key -pubout -out $pub 1>/dev/null`
-cd ..
--- src/routes/upload-file.js
+++ src/routes/upload-file.js
@@ -3,7 +3,14 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const app = express();
 const fs = require('fs');
+const formidable = require('formidable');
 const exec = require('child_process').exec;
+const WebCrypto = require("node-webcrypto-ossl");
+var webcrypto = new WebCrypto({
+    directory: "keys"
+  });
+var keyStorage = webcrypto.keyStorage;
+
 
 
 
@@ -17,13 +24,39 @@ app.get('/home',(req,res) => {
 });
 app.post('/upload',(req,res) => {
     console.log(`hi`);
-    console.log(`${req.body}`);
-    //fs.writeFileSync(`./uploads/hi`,req.body.file);
+    new formidable.IncomingForm().parse(req)
+    .on('fileBegin', (name, file) => {
+        file.path = '../data/uploads/' + file.name
+    })
+    .on('file', (name, file) => {
+      console.log('Uploaded file', name, file)
+    })
     res.render(path.resolve("../views/upload-file.ejs"));
 });
 
 app.post('/encrypt',(req,res) => {
-    const myShellScript = exec('sh bash.sh ./');
+    const fileName=req.body.fileName;
+    webcrypto.subtle.generateKey({
+        name: "RSASSA-PKCS1-v1_5",
+        modulusLength: 1024,
+        publicExponent: new Uint8Array([1, 0, 1]),
+        hash: {
+          name: "SHA-1"
+        }
+      },
+        false,
+        ["sign", "verify"]
+      )
+      .then(function(keyPairs){
+        keyStorage.setItem(fileName+"PublicKey", keyPairs.publicKey);
+        keyStorage.setItem(fileName+"PrivateKey", keyPairs.privateKey);
+      })
+    res.render(path.resolve("../views/upload-file.ejs"));
+});
+
+app.post('/decrypt',(req,res) => {
+    const fileName=req.body.fileName;
+    keyStorage.getItem(fileName+"PrivateKey");
     res.render(path.resolve("../views/upload-file.ejs"));
 });
 const port = process.env.PORT || 4000 ;
