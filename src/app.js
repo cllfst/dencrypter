6ab9d7618a7281b8b5b90ad299d8460920b4dfd8
bouri64 <bourimedali25@gmail.com>
2019-11-03 14:07:28 +0100

    separated css, generalized encryption process to all types of file

--- src/app.js
+++ src/app.js
@@ -4,10 +4,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- src/routes/upload-file.js
+++ src/routes/upload-file.js
@@ -8,65 +8,71 @@ var RSA = require('hybrid-crypto-js').RSA;
 var Crypt = require('hybrid-crypto-js').Crypt;
 var crypt = new Crypt();
 var rsa = new RSA({
-    keySize: 2048,
-    rsaStandard: 'RSA-OAEP', // RSA-OAEP or RSAES-PKCS1-V1_5,
+    keySize: 2048, 
+    rsaStandard: 'RSA-OAEP',
 });
+
+app.use(express.static(path.join(__dirname, '../public')));
+app.use(bodyParser.urlencoded());
 
 app.set('view engine', 'ejs');
 
 
-app.use(bodyParser.urlencoded());
-app.get('/home',(req,res) => {
-    res.render(path.resolve("../views/upload-file.ejs"));
+
+app.get('/home', (req, res) => {
+    userName = 'Mark';
+    res.render(path.join(__dirname, "../views/upload-file.ejs"), {
+        user : userName,
+    });
 });
-app.post('/upload',(req,res) => {
+app.post('/upload', (req, res) => {
     console.log(`hi`);
     new formidable.IncomingForm().parse(req)
     .on('fileBegin', (name, file) => {
-        file.path = '../data/uploads/' + file.name
+        file.path = path.join(__dirname, '../data/uploads/' + file.name)
     })
     .on('file', (name, file) => {
       console.log('Uploaded file', name, file)
     })
-    res.render(path.resolve("../views/upload-file.ejs"));
+    res.render(path.join(__dirname, "../views/upload-file.ejs"));
 });
 
-app.post('/encrypt',(req,res) => {
+app.post('/encrypt', (req, res) => {
     const fileName=req.body.fileName;
     rsa.generateKeyPair(function(keyPair) {
         // Callback function receives new key pair as a first argument
         var publicKey = keyPair.publicKey;
         var privateKey = keyPair.privateKey;
         
-        // data =  fs.readFileSync("../data/uploads/"+fileName); 
-        data = "a test"; 
+        data =  fs.readFileSync("../data/uploads/"+fileName, "binary"); 
         console.log("data : "+data);
         encrypted = crypt.encrypt(publicKey, data);
 
         //saving encrypted data
-        fs.writeFileSync("../data/encrypted/"+fileName+"Encrypted",encrypted);
+        fs.writeFileSync("../data/encrypted/"+fileName+"Encrypted", encrypted, 'binary');
        
         //saving keys
-        fs.writeFileSync("../data/keys/"+fileName+"PublicKey.pem",publicKey);
-        fs.writeFileSync("../data/keys/"+fileName+"PrivateKey.pem",privateKey);
+        fs.writeFileSync("../data/keys/"+fileName+"PublicKey.pem", publicKey);
+        fs.writeFileSync("../data/keys/"+fileName+"PrivateKey.pem", privateKey);
     });
 
 
-    res.render(path.resolve("../views/upload-file.ejs"));
+    res.render(path.join(__dirname, "../views/upload-file.ejs"));
 
 });
 
-app.post('/decrypt',(req,res) => {
+app.post('/decrypt', (req, res) => {
     const fileName=req.body.fileName;
     // iff error??
-    encrypted = fs.readFileSync("../data/encrypted/"+fileName+"Encrypted");
+    encrypted = fs.readFileSync("../data/encrypted/"+fileName+"Encrypted", "binary");
+    console.log("" + encrypted)
     privateKey = fs.readFileSync("../data/keys/"+fileName+"PrivateKey.pem");
-    decrypted = crypt.decrypt("" + privateKey, ""+encrypted);
-    console.log("decrypted : "+decrypted)
-    fs.writeFileSync("../data/uploads/"+fileName+"Decrypted",decrypted);
+    decrypted = crypt.decrypt("" + privateKey, encrypted);
+    console.log("decrypted : "+decrypted.message)
+    fs.writeFileSync("../data/uploads/Decrypted"+fileName, decrypted.message, 'binary');
 
     res.render(path.resolve("../views/upload-file.ejs"));
 });
-const port = process.env.PORT || 4000 ;
+const port = process.env.PORT || 3000 ;
 
 app.listen(port, () => console.log(`listening on port ${port} `));
